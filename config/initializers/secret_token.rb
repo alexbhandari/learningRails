cd5f9e8781a02bafb8a3dc5e5549fe499b0280ad
alexbhandari <alexbhandari@gmail.com>
2013-12-20 20:57:07 -0500

    Initialization and rvm setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'a5ef6c18a52cd1973a76901bf7b84e5f26bea1e5431abf361c4d6988d180809c8e0c01b729a433e63d33ee7efe42b5bae88098fd0e34be05b86f73e4ebd96a47'
