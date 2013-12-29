e59b51a7590a8bb53200acbf404a95d5f2790f84
alexbhandari <alexbhandari@gmail.com>
2013-12-29 17:27:06 -0500

    Email regex updated, about to start ch7

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,9 +32,10 @@ describe User do
    end
 
    describe "when email format is invalid" do
-      it "should be valid" do
+      it "should be invalid" do
          addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                        foo@bar_baz.com foo@bar+baz.com]
+                        foo@bar_baz.com foo@bar+baz.com foo@bar..com
+                        foo@bar........com]
          addresses.each do |invalid_address|
             @user.email = invalid_address
             expect(@user).to_not be_valid
@@ -60,6 +61,15 @@ describe User do
          user_with_same_email.save
       end
       it { should_not be_valid }
+   end
+
+   describe "when email address has mixed case" do
+      let(:mixed_case_email) { "Foo@ExaMPlE.cOM" }
+      it "should be saved in lower-case" do
+         @user.email = mixed_case_email
+         @user.save
+         expect(@user.reload.email).to eq mixed_case_email.downcase
+      end
    end
 
    describe "when password is not present" do
