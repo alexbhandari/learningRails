9b504ec30ab24dad97779bbca4b56582765af747
alexbhandari <alexbhandari@gmail.com>
2013-12-31 02:24:49 -0500

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ActiveRecord::Base
                     length: { maximum: 50 }
 
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-   validates :email, presence: true, 
+   validates :email, presence: true, length: { maximum:50 },
                      format: { with: VALID_EMAIL_REGEX },
                      uniqueness: { case_sensitive: false }
 
    has_secure_password
    validates :password, length: { minimum: 6 }
+   validates :password_confirmation, presence: true
 
 end
